9017ddd6455e0c470201af3d72e18df72e14bda8
Rexe <barmonrohit216@gmail.com>
2026-03-15 02:28:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 clear
-echo "Installing REXE OSINT v2"
+echo "Installing CODER OSINT TOOL v2..."
 
 pkg update -y
+pkg upgrade -y
+
 pkg install python -y
 pkg install git -y
+pkg install curl -y
+pkg install dnsutils -y
 
-pip install -r requirements.txt
+pip install requests
+pip install colorama
+pip install dnspython
 
 chmod +x rexe.py
 
-echo "Run tool:"
+echo ""
+echo "Installation Complete!"
+echo ""
+echo "Run Tool Using:"
 echo "python rexe.py"
