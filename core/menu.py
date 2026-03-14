237082bd24e616916ffdb0343c6c8ad0bfa7d027
Rexe <barmonrohit216@gmail.com>
2026-03-15 02:10:25 +0530

    Update menu.py

--- core/menu.py
+++ core/menu.py
@@ -1,4 +1,10 @@
-from modules import username_search, ip_tracker, email_breach
+from modules import username_search
+from modules import ip_tracker
+from modules import email_breach
+from modules import phone_lookup
+from modules import website_scanner
+from modules import dns_lookup
+from modules import port_scanner
 
 def menu():
 
@@ -13,7 +19,7 @@ def menu():
 [0] Exit
 """)
 
-    choice = input("Select: ")
+    choice = input("Select option: ")
 
     if choice == "1":
         username_search.search()
@@ -23,3 +29,18 @@ def menu():
 
     elif choice == "3":
         email_breach.check()
+
+    elif choice == "4":
+        phone_lookup.lookup()
+
+    elif choice == "5":
+        website_scanner.info()
+
+    elif choice == "6":
+        dns_lookup.lookup()
+
+    elif choice == "7":
+        port_scanner.scan()
+
+    elif choice == "0":
+        exit()
